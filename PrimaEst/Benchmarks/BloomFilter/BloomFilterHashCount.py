59a2f26d62527000722a2b45454c417b5366e003
Pavel Chursin <uber42e@gmail.com>
2020-10-22 20:24:57 +0300

    [BloomFilter] Benchmark

--- PrimaEst/Benchmarks/BloomFilter/BloomFilterHashCount.py
+++ PrimaEst/Benchmarks/BloomFilter/BloomFilterHashCount.py
@@ -10,24 +10,14 @@ import numpy as np
 import math
 from pylab import meshgrid
 
-def memory(capacity, errorProbably):
-    bitsetSize = -((capacity * np.log(errorProbably / 100)) / 0.4761)
-    return (bitsetSize / capacity) * 0.1732
+def hashcount(errorProbably):
+    return -np.log2(errorProbably)
 
-capacities = np.arange(11, 1000000, 19999.78)
-probabilities = np.arange(1, 99, 1.96)
+probabilities = np.arange(0.1, 1, 0.01)
 
-x, y = meshgrid(capacities, probabilities)
 
-z = memory(x, y)
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(x, y, z, rstride=1, cstride=1,vmin=0, vmax=np.nanmax(z), 
-                       linewidth=0, cmap=plt.cm.RdBu, antialiased=True)
-
-ax.set_xlabel('Элементы')
-ax.set_ylabel('Вероятность')
-ax.set_zlabel('Хэш функции')
-fig.colorbar(surf, shrink=1, aspect=10, fraction=.1, panchor=(1.0, 0.0))
+plt.xlabel("Вероятность", fontsize=16)
+plt.ylabel("Количество хэш функций", fontsize=16)
+plt.plot(probabilities, hashcount(probabilities), label="")
+plt.savefig("./BloomFilterHashCount.png")
 plt.show()
--- PrimaEst/Benchmarks/BloomFilter/BloomFilterSelection.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLOOM_FILTER_SELECTION_H
-#define BLOOM_FILTER_SELECTION_H
-
-VOID
-BloomFilterProbablySelection();
-
-#endif
